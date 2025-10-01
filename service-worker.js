f9c20fdfcd0e65b677e86d77214d640fe4e331c7
Golias349 <81453401+Golias349@users.noreply.github.com>
2025-10-01 09:18:28 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,1 @@
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open('gd-v1').then(c=>c.addAll([
-    './','./index.html','./estilo.css','./app.js','./icone-192.png','./icone-512.png','./manifest.json'
-  ])));
-});
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
-});
+self.addEventListener('install',e=>{e.waitUntil(caches.open('gd').then(c=>c.addAll(['./','index.html','estilo.css','app.js','manifest.json'])))})
